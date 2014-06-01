c96ffbb3d2d8a3a5a26564edc61ab2beaa096bfd
Dwi Ardi Irawan <dwiardiirawan@gmail.com>
2014-06-02 05:38:59 +0700

    add spring boot configuration

---
+++ src/main/java/com/makeasoup/edashboard/Application.java
@@ -0,0 +1,15 @@
+package com.makeasoup.edashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@ComponentScan
+@EnableWebMvc
+@EnableAutoConfiguration
+public class Application {
+	public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
